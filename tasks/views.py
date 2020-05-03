9ca41dd7dfd954f57bd689e80917d309f2f3447d
koniaka <volodbka90@gmail.com>
2020-05-03 21:41:03 +0300

    upgrade

--- tasks/views.py
+++ tasks/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from tasks.models import TodoItem, Category, Priority
+from django.views.generic.base import View
 from datetime import datetime
 from django.core.cache import cache
-
 
 def index(request):
     categories = Category.objects.all()
     priorities = Priority.objects.all()
-    return render(request, "tasks/index.html", {"categories": categories, "priorities": priorities})
+    return render(request, "index.html", {"categories": categories, "priorities": priorities,})
 
 def tasks_by_cat(request, cat_slug=None):
     user = request.user
@@ -21,24 +21,22 @@ def tasks_by_cat(request, cat_slug=None):
     categories = Category.objects.all()
     return render(
         request,
-        "tasks/list_by_cat.html",
+        "list_by_cat.html",
         {"category": cat, "tasks": tasks, "categories": categories},
     )
-
 
 class TaskListView(ListView):
     model = TodoItem
     context_object_name = "tasks"
-    template_name = "tasks/list.html"
-
+    template_name = "list.html"
 
 class TaskDetailsView(DetailView):
     model = TodoItem
-    template_name = "tasks/details.html"
+    template_name = "details.html"
 
 def get_cashed_time(request):
     timenow = datetime.now().strftime("%d-%m-%Y / %H:%M:%S")
     if not cache.get('cached_time'):
         cache.set('cached_time', timenow, 300)
-    return render(request, "tasks/datetime_cache.html", {'current_time': timenow,
+    return render(request, "cached_time.html", {'current_time': timenow,
                                                 'cached_time': cache.get('cached_time'), })
